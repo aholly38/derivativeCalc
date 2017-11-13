0f88cd92d57b88033a48accd93530462a7683cf5
Alain Holly <Alain@alain-2.local>
2017-11-12 19:17:19 -0500

    Initial Commit

---
+++ DerivativeCalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DerivativeCalculator
+//
+//  Created by Alain on 11/12/17.
+//  Copyright © 2017 Alain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DerivativeCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DerivativeCalculator
+//
+//  Created by Alain on 11/12/17.
+//  Copyright © 2017 Alain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
