5415a701c9ffc0d31d442af3c63925d0795506cf
Alain Holly <Alain@alain-2.local>
2017-11-14 19:44:28 -0500

    version 1, for class assignment Creating a app that performs derivative functions.

--- DerivativeCalculator/ViewController.h
+++ DerivativeCalculator/ViewController.h
@@ -7,8 +7,36 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <math.h>
 
 @interface ViewController : UIViewController
+
+{
+   double coefficient;
+   double variable;
+   double result;
+    int expo;
+    
+}
+
+@property(nonatomic)NSString *answer;
+@property(nonatomic)NSString *entered;
+
+@property(weak, nonatomic)IBOutlet UILabel *resultLabel;
+@property(weak, nonatomic)IBOutlet UILabel *variableLabel;
+@property (weak, nonatomic) IBOutlet UIButton *calculateButton;
+@property (weak, nonatomic) IBOutlet UIButton *clearButton;
+@property (weak, nonatomic) IBOutlet UIButton *chainFunctionButton;
+
+@property(weak, nonatomic)IBOutlet UITextField *variableTextfield;
+
+@property(weak, nonatomic)IBOutlet UITextField *coefficientTextfield;
+
+- (IBAction)performCalcualtion:(id) sender;
+- (IBAction)clearFields:(id)sender;
+- (void)powerFunction;
+
+
 
 
 @end
--- DerivativeCalculator/ViewController.m
+++ DerivativeCalculator/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <math.h>  
 
 @interface ViewController ()
 
@@ -16,7 +17,31 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    _calculateButton.layer.borderWidth = 2.0f;
+    _calculateButton.layer.cornerRadius = 10;
+    _calculateButton.clipsToBounds = YES;
+    _calculateButton.layer.borderColor = [UIColor blueColor].CGColor;
+    
+    _clearButton.layer.borderWidth = 2.0f;
+    _clearButton.layer.cornerRadius = 10;
+    _clearButton.clipsToBounds = YES;
+    _clearButton.layer.borderColor = [UIColor blueColor].CGColor;
+    
+    _coefficientTextfield.layer.borderWidth = 1.5f;
+    _coefficientTextfield.layer.borderColor = [UIColor blackColor].CGColor;
+   
+    
+    
+    _variableTextfield.layer.borderWidth = 1.5f;
+    _variableTextfield.layer.borderColor = [UIColor blackColor].CGColor;
+    
+    _chainFunctionButton.layer.borderWidth = 2.0f;
+    _chainFunctionButton.layer.cornerRadius = 10;
+    _chainFunctionButton.clipsToBounds = YES;
+    _chainFunctionButton.layer.borderColor = [UIColor blueColor].CGColor;
+
+    
 }
 
 
@@ -24,6 +49,50 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (IBAction)performCalcualtion:(id) sender{
+    
+    [self powerFunction];
+    
+}
+
+- (IBAction)clearFields:(id)sender{
+    
+    _variableTextfield.text = @"";
+    _coefficientTextfield.text = @"";
+    _resultLabel.text = @"0";
+    _variableLabel.text = @"0";
+    
+}
+
+- (IBAction)performChainFunction:(id)sender{
+    
+    [self chainRule];
+    
+}
+- (void)powerFunction{
+    
+    coefficient = ([_coefficientTextfield.text doubleValue]);
+    variable = ([_variableTextfield.text doubleValue]);
+
+    result = pow(coefficient, variable);
+    _answer = [NSString stringWithFormat:@"%.0f", result];
+    _resultLabel.text = _answer;
+    _variableLabel.text = [NSString stringWithFormat:@"%.0f x^%.0f", coefficient, variable];
+    
+}
+
+- (void)chainRule {
+    
+    coefficient = ([_coefficientTextfield.text doubleValue]);
+    variable = ([_variableTextfield.text doubleValue]);
+    
+    result = coefficient * variable ;
+    expo = variable - 1;
+    _answer =[NSString stringWithFormat:@"%.0f x^%.0d", result, expo];
+    _resultLabel.text = _answer;
+}
+
 
 
 @end
